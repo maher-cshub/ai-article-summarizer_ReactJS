4592d37941ea5247662867ac980502aaa11eabe0
maher-cshub <maherarab.1299@gmail.com>
2024-04-29 07:32:36 +0100

    project setup with vite-redux/toolkit - tailwind - and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import MainContent from "./components/MainContent";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Hero />
+      <MainContent />
+    </main>
+  );
+}
+
+export default App;
